3653d98a5be72c37ed3ae759847e94bd50a9d085
Alejandro Nieto <nietsalex@gmail.com>
2026-08-28 21:05:35 +0200

    pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: "export",
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? "/evolve" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
